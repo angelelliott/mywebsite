e3e2d4794f178b395a006339c502be7396ff50f3
angel <aarodriguezulloa@gmail.com>
2025-08-11 14:57:40 -0700

    added all files

---
+++ js/index.js
@@ -0,0 +1,15 @@
+import { ScrambleTextPlugin } from "/node_modules/gsap/ScrambleTextPlugin.js";
+import { Draggable } from "/node_modules/gsap/Draggable.js";
+
+gsap.registerPlugin(ScrambleTextPlugin, Draggable);
+
+const Title = document.querySelector("#angel")
+
+let tl = gsap.timeline();
+
+tl
+  .to(Title, { duration: 0.5, scrambleText:{chars: '˖⊹₊⟡',  text: '꒰ა ANGEL ໒꒱'}}, "+=1")
+  .to(Title, {rotation: -360, duration: 1, ease: "elastic"} , "+=0.8" )
+  .to('.buttons', {duration: 2, opacity:1});
+
+Draggable.create(Title, {type: "rotation",});
