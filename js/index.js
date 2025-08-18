4e4b35873c7368eb4fc132cf7bd4469ecec7af89
angel <aarodriguezulloa@gmail.com>
2025-08-18 16:33:59 -0700

    made changes

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ let tl = gsap.timeline();
 
 tl
   .to(Title, { duration: 0.5, text: '꒰ა ANGEL ໒꒱', ease: "none"}, "+=1")
-  .to(Title, {rotation: -360, duration: 1, ease: "elastic"} , "+=.5" )
-  .to('.buttons', {duration: 2, opacity:1});
+  .to(Title, {rotation: -360, duration: 1, ease: "elastic"}  )
+  .to('.buttons', {duration: 1, opacity:1});
 
 Draggable.create(Title, {type: "rotation",});
