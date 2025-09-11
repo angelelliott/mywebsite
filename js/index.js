f86a2a477c8ca4a8641c96ad482e5dc1dd718410
angel <aarodriguezulloa@gmail.com>
2025-09-11 13:53:20 -0700

    updated files

--- js/index.js
+++ js/index.js
@@ -11,6 +11,6 @@ tl
   .from(Title, { scale: 0, opacity: 0, x: -300, y: -300, duration: 2, ease: Power1.easeIn })  
   .to(Title, { duration: 1.5, text: '⋆ANGEL⋆', ease: "none"},)
   // .to(Title, {duration: 1, scale:0 , ease: Power1.easeIn})
-  .to('.buttons', {duration: 1, opacity:1});
+  .from('.buttons', {duration: 1, opacity:0});
 
-Draggable.create(Title, {type: "rotation",});
+// Draggable.create(Title, {type: "rotation",});
