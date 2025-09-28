2ddd7c0e9280ca6c2d88e91d117fa9d5990839ad
angel <aarodriguezulloa@gmail.com>
2025-09-28 15:50:24 -0700

    changed website colors

--- js/title.js
+++ js/title.js
@@ -4,8 +4,7 @@ let tl = gsap.timeline();
 
 tl
   .from(Title, { scale: 0, opacity: 0, x: -300, y: -300, duration: 2, ease: Power1.easeIn })  
-  .to(Title, { duration: 1.5, text: '⋆ANGEL⋆', ease: "none"},)
-  // .to(Title, {duration: 1, scale:0 , ease: Power1.easeIn})
+  .to(Title, { duration: 1.5, text: 'ᝰANGEL.', ease: "none"},)
   .from('.buttons', {duration: 1, opacity:0});
 
 // Draggable.create(Title, {type: "rotation",});
