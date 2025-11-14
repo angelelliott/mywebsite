5349d92e830200d811b81b36b5140bf401d37f5e
angel <aarodriguezulloa@gmail.com>
2025-11-14 01:26:29 -0800

    made a ton of changes

--- js/title.js
+++ js/title.js
@@ -2,9 +2,6 @@ const Title = document.querySelector("#angel")
 
 let tl = gsap.timeline();
 
-tl
-  .from(Title, { scale: 0, opacity: 0, x: -300, y: -300, duration: 2, ease: Power1.easeIn })  
-  .to(Title, { duration: 1.5, text: 'ᝰANGEL.', ease: "none"},)
-  .from('.buttons', {duration: 1, opacity:0});
+tl;
 
 // Draggable.create(Title, {type: "rotation",});
