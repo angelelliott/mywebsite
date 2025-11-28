01bc931cb5b9888caf77f0b2b35c49286c9fc296
angel <aarodriguezulloa@gmail.com>
2025-11-28 00:32:25 -0800

    animated title

--- js/title.js
+++ js/title.js
@@ -1,6 +1,8 @@
-const Title = document.querySelector("#angel")
-
-let tl = gsap.timeline();
-
-
-// Draggable.create(Title, {type: "rotation",});
+var ztxt = new Ztextify(".angel", {
+   depth: "3rem",
+   layers: 10,
+   fade: false,
+   direction: "forwards",
+   event: "pointer",
+   eventRotation: "10deg"
+});
