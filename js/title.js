7044f21e3ca0be5e8cf5df443495af15bb1321b1
angel <aarodriguezulloa@gmail.com>
2026-05-13 20:41:01 -0700

    angel

--- js/title.js
+++ js/title.js
@@ -6,3 +6,11 @@ var ztxt = new Ztextify(".angel", {
    event: "pointer",
    eventRotation: "10deg"
 });
+
+
+// And this is a Timeline, containing three sequenced tweens
+let tl = gsap.timeline();
+
+tl.from("#angel", {duration: 1, opacity: 0, rotateZ:-10}).delay(1)
+  .from("#elliott", {duration: 1, opacity:0, rotateZ:10})
+  .from("#ulloa", {duration: 1, opacity:0, rotateZ:-10});
